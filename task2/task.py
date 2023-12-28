6bfb00f295d8fe8c1306a0aacf17890e25f995b5
Timofey Drenkov <drenkov2002@yandex.ru>
2023-12-28 21:52:04 +0300

    refactored task 2

--- task2/task.py
+++ task2/task.py
@@ -1,21 +1,28 @@
 import csv
-import sys
+import io
 
-def load_graph(csv_file):
+EXAMPLE_INPUT = '''1,2
+2,3
+3,4
+3,5
+4,6'''
+
+def load_graph(csv_string):
     graph_data = {}
-    with open(csv_file, 'r') as file:
-        reader = csv.reader(file)
-        for line in reader:
-            node = line[0]
-            edges = line[1:]
-            graph_data.setdefault(node, []).extend(edges)
+    reader = reader = csv.reader(io.StringIO(csv_string))
+    for line in reader:
+        node = line[0]
+        edges = line[1:]
+        graph_data.setdefault(node, []).extend(edges)
     return graph_data
+
 
 def extract_nodes(graph):
     nodes = set(graph.keys())
     for edges in graph.values():
         nodes.update(edges)
     return sorted(nodes), len(nodes)
+
 
 def create_adjacency_matrix(graph, size):
     adj_matrix = [[0] * size for _ in range(size)]
@@ -26,6 +33,7 @@ def create_adjacency_matrix(graph, size):
             adj_matrix[node_index][edge_index] = 1
             adj_matrix[edge_index][node_index] = -1
     return adj_matrix
+
 
 def analyze_graph(matrix):
     analysis = [[0] * 5 for _ in matrix]
@@ -40,20 +48,22 @@ def analyze_graph(matrix):
                 analysis[i][4] += sum(1 for k, v in enumerate(matrix[j]) if v == 1 and k != i)
     return analysis
 
-def save_to_csv(data, filename='output.csv'):
+
+def save_to_csv(data, filename='result.csv'):
     with open(filename, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerows(data)
 
-def process_file(file_path):
-    graph = load_graph(file_path)
+
+def task(csv):
+    graph = load_graph(csv)
     _, size = extract_nodes(graph)
     adjacency_matrix = create_adjacency_matrix(graph, size)
     result_data = analyze_graph(adjacency_matrix)
-    save_to_csv(result_data)
+    return result_data
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python task.py <file_path>")
-        sys.exit(1)
-    process_file(sys.argv[1])
+
+if __name__ == '__main__':
+    csv_input = EXAMPLE_INPUT
+    result = task(csv_input)
+    print(result)
